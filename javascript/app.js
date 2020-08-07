c664ba03116440b15c85fa8767cc9b9ddd804dca
nikhil teotia <50858608+nikhilteotia@users.noreply.github.com>
2020-08-07 14:34:31 +0530

    updates

--- javascript/app.js
+++ javascript/app.js
@@ -5,16 +5,17 @@ const navSlide = () => {
   const nav = document.querySelector(".nav-links");
   const navLinks = document.querySelectorAll(".nav-links li");
   var hamImg = document.getElementById("ham-img");
+  const server = "http://127.0.0.1:5500";
 
   hamMenu.addEventListener("click", () => {
     nav.classList.toggle("nav-active");
 
     //change of images is here note:: change path of sever with the current directory or with __dir if on server.
 
-    if (hamImg.src == "/images/icons/ham-menu.svg") {
-      hamImg.src = "/images/icons/cross.png";
+    if (hamImg.src == server + "/images/icons/ham-menu.svg") {
+      hamImg.src = server + "/images/icons/cross.png";
     } else {
-      hamImg.src = "/images/icons/ham-menu.svg";
+      hamImg.src = server + "/images/icons/ham-menu.svg";
     }
 
     // animation of navbar is here
